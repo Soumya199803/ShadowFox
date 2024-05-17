9d82ac4ed2aebcd60641b429ef8a944fd802ed2a
Soumya199803 <159017119+Soumya199803@users.noreply.github.com>
2024-05-17 21:50:46 +0530

    port.php

---
+++ port.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Junk food</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/final year prjt/favicon_io.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/final year prjt/favicon_io/favicon-32x32.png">
+<!--link rel="icon" type="image/png" sizes="16x16" href="/final year prjt/favicon_io/favicon-16x16.png"-->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+/* Content */
+.content { 
+  background-color: black;
+  padding: 70px;
+  margin: 10px -16px;
+  font-color:black;
+  
+  
+}
+
+
+
+
+<!-- SIDE BAR CODE-->
+body {
+  font-family: "Lato", sans-serif;
+  
+}
+
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #FFDFBA;
+  overflow-x: hidden;
+  padding-top: 10px;
+}
+
+.sidenav a {
+  padding: 9px 8px 9px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: black;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: black;
+}
+
+.main {
+  margin-left: 160px; /* Same as the width of the sidenav */
+  font-size: 28px; /* Increased text to enable scrolling */
+  padding: 0px 20px;
+}
+
+@media screen and (max-height: 50px) {
+  .sidenav {padding-top: 1px;}
+  .sidenav a {font-size: 18px;}
+}
+
+
+
+
+body{
+	background-image: url("background7.jpg");
+	
+}
+</style>
+</head>
+<body>
+
+ 
+<div class="content">
+
+  <center>
+  <img src="images/p10.jpg" alt="Bear" width="400" height="400" ></center>
+ <center> 
+ <h3><b>SOUMYA IPPALPALLI<br>COMPUTER SCIENCE ENGINEERING STUDENT</b></h3></center>
+ <center>
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ 
+ 
+ 
+ 
+ <a href="home1.php"><button type="button" class="btn btn-primary">Contact me</button></a>
+ </center>
+ 
+  </div>
+
+
+<div class="sidenav">
+  <a href="#"><b>MENU:</b></a><BR><br>
+  <a href="educational_info.php"><b><I>Educational Information:</I></b></a><br>
+  <a href="aboutme.php"><b><I>About me:</I></b></a><BR>
+  <a href="certifications.php"><b><I>Certifications:</I></b></a><BR>
+  
+  
+ 
+ 
+</div>  
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</html> 
