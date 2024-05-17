a6536172ac78556fa4c58e3cd1f7b211827515cb
Soumya199803 <159017119+Soumya199803@users.noreply.github.com>
2024-05-17 21:52:49 +0530

    certifications.php

---
+++ certifications.php
@@ -0,0 +1,147 @@
+<html>
+<head>
+<title>Curing</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/final year prjt/favicon_io.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/final year prjt/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/final year prjt/favicon_io/favicon-16x16.png">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<style>
+
+<!-- padding images-->
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+  padding: 20px;
+  font-family: Arial;
+}
+
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+h1 {
+  font-size: 50px;
+  word-break: break-all;
+}
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 25%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 10px;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 900px) {
+  .column {
+    width: 50%;
+  }
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+
+
+
+img{
+   border: 5px solid#000000
+}   
+
+body{
+	background-color:#FFDFBA;
+}
+
+</style>
+</head>
+<body>
+
+
+<center>
+<h1 style="font-size:65px; background-color:black;color:white;">CERTIFICATIONS</h1>
+</center>
+
+
+<br>
+<br>
+<ul>
+<li><h2>Successfully Completed internship at CodSoft of Java Programming</h2></li><br>
+<li><h2>Successfully Completed internship at Oasis InfoByte of Web Development and Designing</h2></li><br>
+<li><h2>Successfully Completed internship at CodSoft of Web Development</h2></li><br>
+<li><h2>Participated Dipex State Level Competition held at Navi-mumbai(Nerul) 2k24</h2></li><br>
+<li><h2>Participated Poster Presentation at Barshi Solapur</h2></li><br>
+<li><h2>Participated Idea Presentation National Level Competition held at Shree Siddeshwar Womens 
+College of Engineering Solapur</h2></li><br>
+</ul>
+<div class="row">
+  <div class="column">
+    <div class="content">
+      <img src="images/a2.jpg" alt="Mountains" style="width:80%">
+      
+    </div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/a3.jpg" alt="Lights" style="width:80%">
+      
+    </div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/a4.jpg" alt="Nature" style="width:80%">
+      
+	  
+	    </div>
+  </div>
+  
+  <div class="column">
+    <div class="content">
+    <img src="images/a2.jpg" alt="Nature" style="width:80%">
+	
+ </div>
+ <br>
+ </div>
+		
+		
+		
+
+
+<li>
+ <center>
+ <a href="port.php"><button type="button" class="btn btn-primary">Back to menu</button></a> 
+ </center>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
