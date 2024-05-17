8e9144cac033797ee75213520585dd33c0bf9b07
Soumya199803 <159017119+Soumya199803@users.noreply.github.com>
2024-05-17 21:51:39 +0530

    educatioal_info.php

---
+++ educatioal_info.php
@@ -0,0 +1,148 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reasons</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/final year prjt/favicon_io.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/final year prjt/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/final year prjt/favicon_io/favicon-16x16.png">
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #FFDFBA;
+  padding: 20px;
+  font-family: Arial;
+}
+
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+h1 {
+  font-size: 50px;
+  word-break: break-all;
+}
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 25%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 10px;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 900px) {
+  .column {
+    width: 50%;
+  }
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+
+
+
+
+</style>
+</head>
+
+<body>
+
+<!-- MAIN (Center website) -->
+<div class="main">
+
+<center>
+<h1 style="font-size:65px; background-color:black;color:white;">EDUCATIONAL INFORMATION</h1>
+</center>
+
+
+<!-- Portfolio Gallery Grid -->
+<div class="row">
+  <div class="column">
+    <div class="content">
+      <img src="images/e1.jpg" alt="Mountains" style="width:80%">
+      
+    </div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/e2.jpg" alt="Lights" style="width:80%">
+      
+    </div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/e3.jpg" alt="Nature" style="width:80%">
+      
+	  
+	    </div>
+  </div>
+  
+  <div class="column">
+    <div class="content">
+    <img src="images/e4.jpg" alt="Nature" style="width:80%">
+      
+	  
+	    </div>
+		<br>
+  </div>
+  <br>
+  
+  <hr>
+  <ul>
+<li><h2>Completed my 10th boards at Padmashree Sumatibai English Medium Technical High school,Solapur</h2></li><br>
+<li><h2>Completed my 12th boards at Walchand College of Arts and Science,Solapur</h2></li><br>
+<li><h2>Pursued diploma of Computer Science Engineering at Shree Siddeshwar Women's Polytechnic,Solapur</h2></li><br>
+<li><h2>Pursing BTech of Computer Science Engineering at Shree Siddeshwar Women's College of Engineering,Solapur</h2></li><br>
+
+<br>
+<br>
+<br>
+<center>
+ <a href="port.php"><button type="button" class="btn btn-primary">Back to Menu</button></a> 
+ </center>
+ 
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</html>
