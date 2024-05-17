19a4f5b76f44c4c6b30603380ddb3ff518fad23b
Soumya199803 <159017119+Soumya199803@users.noreply.github.com>
2024-05-17 21:52:17 +0530

    aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<title>Prevention</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/final year prjt/favicon_io.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/final year prjt/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/final year prjt/favicon_io/favicon-16x16.png">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<style>
+body{
+	background-color:#FFDFBA;
+}
+
+img{
+	border: 5px solid #000000;
+}
+
+
+
+</style>
+</head>
+
+<body>
+
+		<center>
+			    <h1 style="font-size:75px; background-color:black;color:white;">ABOUT ME</h1>
+		</center>
+		<br>
+		<br>
+		
+		<img src="images/a1.jpg" align="right" height="300" width="450">&nbsp;&nbsp;
+<ul>
+<li><h2>Completed my 10th boards at Padmashree Sumatibai English Medium Technical High school,Solapur</h2></li><br>
+<li><h2>Completed my 12th boards at Walchand College of Arts and Science,Solapur</h2></li><br>
+<li><h2>Pursued diploma of Computer Science Engineering at Shree Siddeshwar Women's Polytechnic,Solapur</h2></li><br>
+<li><h2>Pursing BTech of Computer Science Engineering at Shree Siddeshwar Women's College of Engineering,Solapur</h2></li><br>
+<li><h2>Successfully Completed internship at CodSoft of Java Programming</h2></li><br>
+<li><h2>Successfully Completed internship at Oasis InfoByte of Web Development and Designing</h2></li><br>
+<li><h2>Successfully Completed internship at CodSoft of Web Development</h2></li><br>
+<li><h2>Participated Dipex State Level Competition held at Navi-mumbai(Nerul) 2k24</h2></li><br>
+<li><h2>Participated Poster Presentation at Barshi Solapur</h2></li><br>
+<li><h2>Participated Idea Presentation National Level Competition held at Shree Siddeshwar Womens 
+College of Engineering Solapur</h2></li><br>
+</ul>
+<br>
+<br>
+
+
+<center>
+ <a href="port.php"><button type="button" class="btn btn-primary">Back to menu</button></a> 
+ </center>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
